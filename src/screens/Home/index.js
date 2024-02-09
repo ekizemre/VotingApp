eca2f2b26d1a5ca523f13e1daee9856533e16d20
emrekiz0 <100236657+rewindyflakes@users.noreply.github.com>
2024-02-09 14:10:17 +0300

    slide update
    updated

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,23 +1,26 @@
-import { StyleSheet, View, Modal } from 'react-native'
-import { Button, Text } from "native-base";
 import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Modal from 'react-native-modal';
 import Questions from '../Questions';
 import AddButton from '../../components/AddButton';
 import AddNewModal from '../Questions/AddNewModal';
 
-
 const Home = ({ navigation }) => {
-
-
     const [modalVisible, setModalVisible] = useState(false);
 
     React.useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
-                <AddButton icon_name={"add-circle-outline"} onPress={() => setModalVisible((prev) => !prev)} />
+                <AddButton
+                    icon_name={'add-circle-outline'}
+                    onPress={() => setModalVisible(true)}
+                />
             ),
             headerLeft: () => (
-                <AddButton icon_name={"person-circle-outline"} onPress={() => navigation.navigate("Profile")} />
+                <AddButton
+                    icon_name={'person-circle-outline'}
+                    onPress={() => navigation.navigate('Profile')}
+                />
             ),
         });
     }, [navigation]);
@@ -25,16 +28,14 @@ const Home = ({ navigation }) => {
         <View>
             <Questions />
             <Modal
-                animationType="slide"
-                visible={modalVisible}
-                presentationStyle={"pageSheet"}
-                onRequestClose={() => setModalVisible(false)}
-            >
+                isVisible={modalVisible}
+                swipeDirection="down" // sayfayı kapatmak için aşağı kaydırabilirsiniz.
+                onSwipeComplete={() => setModalVisible(false)}
+                style={{ justifyContent: 'flex-end', margin: 0 }}>
                 <AddNewModal closeModal={() => setModalVisible(false)} />
             </Modal>
         </View>
     );
 };
 
-export default Home
-
+export default Home;
